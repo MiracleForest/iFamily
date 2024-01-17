a25a4fb565449956ed75a29570f012f5c9d08497
Lovelylavender4 <MiracleForest@Outlook.com>
2024-01-17 17:43:55 +0800

    添加项目文件。

---
+++ iFamily/tests/iFamilyTest.cpp
@@ -0,0 +1,10 @@
+#include <imacrofamily.h>
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
---
+++ xmake.lua
@@ -0,0 +1,32 @@
+add_rules("mode.debug", "mode.release")
+
+target("iFamilyTests")
+    set_kind("binary")
+    add_files("iFamily/tests/**.cpp")
+    add_headerfiles(
+        "iFamily/include/**.h"
+    )
+    add_includedirs(
+        "iFamily/include"
+    )
+    add_cxxflags(
+        "/utf-8",
+        "/permissive-",
+        "/EHa",
+        "/W4",
+        "/w44265",
+        "/w44289",
+        "/w44296",
+        "/w45263",
+        "/w44738",
+        "/w45204"
+    )
+    add_defines(
+        "_CRT_SECURE_NO_WARNINGS",
+        "_ENABLE_CONSTEXPR_MUTEX_CONSTRUCTOR",
+        "NOMINMAX",
+        "UNICODE",
+        "WIN32_LEAN_AND_MEAN"
+    )
+    set_languages("c++20")
+target_end()
