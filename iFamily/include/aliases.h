1127855c7972a69467655e257ec95e3eb36227ba
Lovelylavender4 <MiracleForest@Outlook.com>
2024-01-28 22:35:33 +0800

    feat: remove the core namespace and add aliases for u8char, u16char, etc.

--- iFamily/include/aliases.h
+++ iFamily/include/aliases.h
@@ -18,112 +18,112 @@
 
 namespace MiracleForest::inline i
 {
-namespace core
-{
+using ushort  = unsigned short;
+using sshort  = signed short;
+using uint    = unsigned int;
+using sint    = signed int;
+using llong   = long long;
+using ulong   = unsigned long;
+using ullong  = unsigned long long;
+using slong   = signed long;
+using sllong  = signed long long;
+using uchar   = unsigned char;
+using schar   = signed char;
+using wchar   = wchar_t;
+using u8char  = char8_t;
+using u16char = char16_t;
+using char8   = u8char;
+using char16  = u16char;
+using int64   = long long;
+using uint64  = unsigned long long;
 
-    using ushort = unsigned short;
-    using sshort = signed short;
-    using uint   = unsigned int;
-    using sint   = signed int;
-    using llong  = long long;
-    using ulong  = unsigned long;
-    using ullong = unsigned long long;
-    using slong  = signed long;
-    using sllong = signed long long;
-    using uchar  = unsigned char;
-    using schar  = signed char;
-    using int64  = long long;
-    using uint64 = unsigned long long;
+using __unk_type_v__ = void*;
+using unkTypev       = void*;
+using unkPtr         = void*;
+using anyPtr         = void*;
+using handle         = void*;
 
-    using __unk_type_v__ = void*;
-    using unkTypev       = void*;
-    using unkPtr         = void*;
-    using anyPtr         = void*;
-    using handle         = void*;
-    using wchar          = wchar_t;
-
-    using _BYTE  = uchar;
-    using _WORD  = ushort;
-    using _DWORD = uint;
-    using _QWORD = uint64;
+using _BYTE  = uchar;
+using _WORD  = ushort;
+using _DWORD = uint;
+using _QWORD = uint64;
 
 #if !defined(_MSC_VER)
 #    define _OWORD __int128
 #endif
 
-    using VA  = uint64;
-    using RVA = uint;
+using VA  = uint64;
+using RVA = uint;
 
-    template<typename _Type>
-    using Ptr = _Type*;
+template<typename _Type>
+using Ptr = _Type*;
 
-    template<typename _Type>
-    using PPtr = _Type**;
+template<typename _Type>
+using PPtr = _Type**;
 
-    template<typename _Type>
-    using CPtr = const _Type*;
+template<typename _Type>
+using CPtr = const _Type*;
 
-    template<typename _Type>
-    using CPPtr = const _Type**;
+template<typename _Type>
+using CPPtr = const _Type**;
 
-    template<typename _Type>
-    using CRef = const _Type&;
+template<typename _Type>
+using CRef = const _Type&;
 
-    template<typename _Type>
-    using CRRef = const _Type&&;
+template<typename _Type>
+using CRRef = const _Type&&;
 
-    template<typename _Type>
-    using Ref = _Type&;
+template<typename _Type>
+using Ref = _Type&;
 
-    template<typename _Type>
-    using RRef = _Type&&;
+template<typename _Type>
+using RRef = _Type&&;
 
-    template<typename _Type>
-    struct stdcall_cast_s;
+template<typename _Type>
+struct stdcall_cast_s;
 
-    template<typename _Type>
-    struct cdecl_cast_s;
+template<typename _Type>
+struct cdecl_cast_s;
 
-    template<typename _Type>
-    struct fastcall_cast_s;
+template<typename _Type>
+struct fastcall_cast_s;
 
-    template<typename _Type>
-    struct thiscall_cast_s;
+template<typename _Type>
+struct thiscall_cast_s;
 
-    template<typename _RetTy, typename... _ArgTy>
-    struct stdcall_cast_s<_RetTy(_ArgTy...)>
-    {
-        using type = _RetTy(__stdcall*)(_ArgTy...);
-    };
+template<typename _RetTy, typename... _ArgTy>
+struct stdcall_cast_s<_RetTy(_ArgTy...)>
+{
+    using type = _RetTy(__stdcall*)(_ArgTy...);
+};
 
-    template<typename _RetTy, typename... _ArgTy>
-    struct cdecl_cast_s<_RetTy(_ArgTy...)>
-    {
-        using type = _RetTy(__cdecl*)(_ArgTy...);
-    };
+template<typename _RetTy, typename... _ArgTy>
+struct cdecl_cast_s<_RetTy(_ArgTy...)>
+{
+    using type = _RetTy(__cdecl*)(_ArgTy...);
+};
 
-    template<typename _RetTy, typename... _ArgTy>
-    struct fastcall_cast_s<_RetTy(_ArgTy...)>
-    {
-        using type = _RetTy(__fastcall*)(_ArgTy...);
-    };
+template<typename _RetTy, typename... _ArgTy>
+struct fastcall_cast_s<_RetTy(_ArgTy...)>
+{
+    using type = _RetTy(__fastcall*)(_ArgTy...);
+};
 
-    template<typename _RetTy, typename... _ArgTy>
-    struct thiscall_cast_s<_RetTy(_ArgTy...)>
-    {
-        using type = _RetTy(__thiscall*)(_ArgTy...);
-    };
+template<typename _RetTy, typename... _ArgTy>
+struct thiscall_cast_s<_RetTy(_ArgTy...)>
+{
+    using type = _RetTy(__thiscall*)(_ArgTy...);
+};
 
-    template<typename _Func>
-    using stdcall_cast = typename stdcall_cast_s<_Func>::type;
+template<typename _Func>
+using stdcall_cast = typename stdcall_cast_s<_Func>::type;
 
-    template<typename _Func>
-    using cdecl_cast = typename cdecl_cast_s<_Func>::type;
+template<typename _Func>
+using cdecl_cast = typename cdecl_cast_s<_Func>::type;
 
-    template<typename _Func>
-    using fastcall_cast = typename fastcall_cast_s<_Func>::type;
+template<typename _Func>
+using fastcall_cast = typename fastcall_cast_s<_Func>::type;
 
-    template<typename _Func>
-    using thiscall_cast = typename thiscall_cast_s<_Func>::type;
-} // namespace core
+template<typename _Func>
+using thiscall_cast = typename thiscall_cast_s<_Func>::type;
 } // namespace MiracleForest::inline i
